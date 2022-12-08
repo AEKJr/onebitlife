ddd07913567f4338479ec8d4dff76e2fa90a8f40
AEKJr <aek.jr@outlook.com>
2022-12-08 19:00:44 -0300

    Botão de Criar Habitos

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { ScrollView, View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+
 import LifeStatus from "../../Components/Common/LifeStatus";
 import StatusBar from "../../Components/Home/StatusBar";
+import CreateHabit from "../../Components/Home/CreateHabit";
 
 export default function Home() {
-        const navigation = useNavigation();
+    const navigation = useNavigation();
+	const [mindHabit, setMindHabit] = useState();
+    const [moneyHabit, setMoneyHabit] = useState();
+    const [bodyHabit, setBodyHabit] = useState();
+    const [funHabit, setFunHabit] = useState();
 
-        function handleNavExplanation() {
-            navigation.navigate("AppExplanation");
-        }
+    function handleNavExplanation() {
+        navigation.navigate("AppExplanation");
+    }
 
     return (
         <View style={styles.container}>
@@ -18,30 +24,33 @@ export default function Home() {
                     <Text style={styles.dailyChecks}>❤️ 20 dias - ✔️ 80 checks</Text>
                     <LifeStatus />
                     <StatusBar />
+                    <CreateHabit habitArea="Mente" borderColor = "#90B7F3"/>
                 </View>
+
                 <Text
                     style={styles.explanationText}
-                    onPress={ () => {
+                    onPress={() => {
                         handleNavExplanation();
                     }}
                 >
-                    Ver explicação novamente</Text>
+                    Ver explicações novamente
+                </Text>
             </ScrollView>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "rgba(21, 21, 21, 0.98)",       
+        backgroundColor: "rgba(21, 21, 21, 0.98)",
     },
     dailyChecks: {
         color: "white",
         fontWeight: "bold",
         textAlign: "center",
         fontSize: 18,
-        marginTop: 40,        
+        marginTop: 40,
     },
     explanationText: {
         color: "white",
@@ -51,4 +60,4 @@ const styles = StyleSheet.create({
         paddingTop: 15,
         paddingBottom: 25,
     },
-})
+});
